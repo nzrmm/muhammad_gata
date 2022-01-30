d6b1277058dccfc63c711ccb3f4fbe674e54f2b8
nzrmm <iamgata.me@gmail.com>
2022-01-30 15:21:41 +0700

    [change] edit layout contact component

--- src/components/template/TheContact.vue
+++ src/components/template/TheContact.vue
@@ -1,20 +1,21 @@
 <template>
   <section class="py-36 bg-dark-700">
     <div class="container">
-      <div class="w-5/12">
-        <h3 class="mb-6 uppercase">Contact Me</h3>
-        <p class="mb-12 text-neutral-400">
-          If you interested with me, Iam available at any time. You can click
-          button bellow to send me an email
+      <div class="flex flex-col justify-center items-center">
+        <p class="w-6/12 mb-2 text-center text-neutral-400">
+          If you interested with me, Iam available at any time. You can link
+          email bellow to send me an email
         </p>
-        <a href="#" class="link-gradient">Email Me</a>
+        <a href="#">
+          <h3 class="leading-relaxed text-gradient">iamgata.me@gmail.com</h3>
+        </a>
       </div>
     </div>
   </section>
 </template>
 
 <style scoped>
-.link-gradient {
-  @apply py-4 px-10 bg-gradient-to-r from-teal-500 to-purple-500 hover:from-purple-500 hover:to-teal-500 rounded-lg transition-all duration-500;
+.text-gradient {
+  @apply bg-gradient-to-r from-teal-500 to-purple-500 bg-clip-text text-transparent;
 }
 </style>
